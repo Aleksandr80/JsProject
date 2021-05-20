e0cf9b415a61bb23ba237894a65cd8243a16df80
Aleks <alexscif2@gmail.com>
2021-05-20 19:21:07 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+"use strict";
+
+// console.log(1);
+let number = 5;
+const leftBorderWidth = 1;
+
+// console.log(something);
+// let und;
+// console.log(und);
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+// console.log(obj["name"]);
+console.log(obj.name);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp', 6, {}, []];
+console.log(arr[1]);
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result); //выводит true или false
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(answer);
+// console.log(typeof(answer)); // По умолчанию всегда строка
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(typeof(answer)); // А сдесь число, так как prompt привели к числу
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+// document.write(answers);
+const category = 'toys';
+// console.log('https://someurl.com/' + category);
+//Интерполяция
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "Ivan";
+alert(`Привет, ${user}`);
