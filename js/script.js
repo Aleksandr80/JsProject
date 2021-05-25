c7fde7d1a941dd650cc1fd4d8a84fbf30e470d7a
Aleks <alexscif2@gmail.com>
2021-05-25 13:38:08 +0300

    first practice

--- js/script.js
+++ js/script.js
@@ -30,11 +30,11 @@ console.log(arr[1]);
 // const answer = +prompt("Вам есть 18?", "18");
 // console.log(typeof(answer)); // А сдесь число, так как prompt привели к числу
 
-const answers = [];
+// const answers = [];
 
-answers[0] = prompt('Как ваше имя?', '');
-answers[1] = prompt('Как ваша фамилия?', '');
-answers[2] = prompt('Сколько вам лет?', '');
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
 
 // document.write(answers);
 const category = 'toys';
@@ -42,5 +42,30 @@ const category = 'toys';
 //Интерполяция
 console.log(`https://someurl.com/${category}/5`);
 
-const user = "Ivan";
-alert(`Привет, ${user}`);
+// const user = "Ivan";
+// alert(`Привет, ${user}`);
+
+// const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?','');
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let lastOfFilms = prompt('Один из последних просмотренных фильмов?',''),
+    costOfFilms = prompt('На сколько оцените его?',''),
+    lastOfFilms2 = prompt('Один из последних просмотренных фильмов?',''),
+    costOfFilms2 = prompt('На сколько оцените его?','');
+
+personalMovieDB.movies[lastOfFilms] = costOfFilms;
+personalMovieDB.movies[lastOfFilms2] = costOfFilms2;
+console.log(personalMovieDB);
+
+
+
+
